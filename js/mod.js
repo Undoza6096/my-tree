b44d3405b89bfb24f144d5390dc7e605f23a88a7
Undoza6096 <106401980+Undoza6096@users.noreply.github.com>
2022-07-02 14:33:54 -0300

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,5 +1,5 @@
 let modInfo = {
-	name: "The ??? Tree",
+	name: "The Tree",
 	id: "mymod",
 	author: "nobody",
 	pointsName: "points",
@@ -43,6 +43,7 @@ function getPointGen() {
 		return new Decimal(0)
 
 	let gain = new Decimal(1)
+        if (hasUpgrade('p', 11)) gain = gain.times(2)
 	return gain
 }
 
